f9e02eb9e9d9f1ccc1b601e1b9e2eede62f113ac
Hitayu Patel <hitayupatel8@gmail.com>
2019-07-16 11:50:55 -0700

    Removed Readme file

---
+++ src/MyVacation.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function MyVacation() {
+  return (
+    <div>
+      <h1>Hitayu Patel</h1>
+      <p>About me and my list</p>
+      <ul>
+        <li>California</li>
+        <li>New Zealand</li>
+        <li>Switzerland</li>
+      </ul>
+    </div>
+  );
+}
+export default MyVacation;
