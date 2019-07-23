fbf0751450b96cb6451da6003cd13ae2052a453f
Hitayu Patel <hitayupatel8@gmail.com>
2019-07-22 17:47:48 -0700

    Tried props

--- src/App.js
+++ src/App.js
@@ -1,14 +1,41 @@
 import React from "react";
-import AwesomeFooter from "./Components/AwesomeFooter";
-import AwesomeHeader from "./Components/AwesomeHeader";
-import MainContent from "./Components/MainContent";
+import ComponentCard from "./Components/ComponentCard";
 
 function App() {
   return (
     <div>
-      <AwesomeHeader />
-      <MainContent />
-      <AwesomeFooter />
+      <ComponentCard
+        contact={{
+          name: "Mr. Whiskerson",
+          imgUrl: "http://placekitten.com/300/200",
+          phone: "(202) 538-1469",
+          email: "mr.whiskaz@catnap.meow"
+        }}
+      />
+      <ComponentCard
+        contact={{
+          name: "Fluffykins",
+          imgUrl: "http://placekitten.com/400/200",
+          phone: "(202) 259-6487",
+          email: "mr.fluff@catty.meow"
+        }}
+      />
+      <ComponentCard
+        contact={{
+          name: "Destroyer",
+          imgUrl: "http://placekitten.com/400/300",
+          phone: "(202) 888-2356",
+          email: "folofworld@catnap.com"
+        }}
+      />
+      <ComponentCard
+        contact={{
+          name: "Mr. Felix",
+          imgUrl: "http://placekitten.com/200/100",
+          phone: "(202) 987-1125",
+          email: "cat@yahoo.com"
+        }}
+      />
     </div>
   );
 }
