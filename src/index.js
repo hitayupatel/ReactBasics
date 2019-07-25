8c636783854a1c3e1e45f6727d088cc6ddf6e4ea
Hitayu Patel <hitayupatel8@gmail.com>
2019-07-25 15:52:15 -0700

    Added Class components

--- src/index.js
+++ src/index.js
@@ -1,9 +1,45 @@
-import React from "react";
+import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./App";
+//import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Header username="abc" />
+        <Greeting />
+      </div>
+    );
+  }
+}
+
+class Header extends React.Component {
+  render() {
+    return (
+      <header>
+        <p>Welcome, {this.props.username}</p>
+      </header>
+    );
+  }
+}
+
+class Greeting extends Component {
+  render() {
+    const date = new Date();
+    const hours = date.getHours();
+    let timeOfDay;
+    if (hours < 12) {
+      timeOfDay = "morning";
+    } else if (hours >= 12 && hours <= 17) {
+      timeOfDay = "afternoon";
+    } else {
+      timeOfDay = "night";
+    }
+    return <h1> Good {timeOfDay} to you. </h1>;
+  }
+}
 ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
