81683e9b1cb85931426c6b645bf6ff64789be822
Hitayu Patel <hitayupatel8@gmail.com>
2019-07-31 11:34:48 -0700

    Made custom Form

--- src/App.js
+++ src/App.js
@@ -7,9 +7,12 @@ class App extends React.Component {
     this.state = {
       firstName: "",
       lastName: "",
-      isFriendly: false,
+      age: "",
       gender: "",
-      favcolor: "blue"
+      destination: "",
+      isVegan: false,
+      isLactoseFree: false,
+      isVegeterain: false
     };
     this.handleChange = this.handleChange.bind(this);
   }
@@ -17,17 +20,12 @@ class App extends React.Component {
   handleChange(event) {
     const { name, value, type, checked } = event.target;
     type === "checkbox"
-      ? this.setState({ [name]: checked })
+      ? this.setState({
+          [name]: checked
+        })
       : this.setState({ [name]: value });
   }
   render() {
-    //const text = this.state.loading ? "loading..." : this.state.character.name;
-    // function handleClick() {
-    //   console.log("I was clicked!!");
-    // }
-    // function handleHover() {
-    //   console.log("Hovered!!cd");
-    // }
     return (
       <form onSubmit={this.handleSubmit}>
         <input
@@ -46,19 +44,14 @@ class App extends React.Component {
           onChange={this.handleChange}
         />
         <br />
-
-        <textarea value={"Some Default value"} onChange={this.handleChange} />
-
+        <input
+          type="text"
+          name="age"
+          value={this.state.age}
+          placeholder="Age"
+          onChange={this.handleChange}
+        />
         <br />
-        <label>
-          <input
-            type="checkbox"
-            name="isFriendly"
-            checked={this.state.isFriendly}
-            onChange={this.handleChange}
-          />
-          Done?
-        </label>
         <br />
         <label>
           <input
@@ -81,53 +74,74 @@ class App extends React.Component {
           Female
         </label>
         <br />
+        <label>
+          Destination:
+          <select
+            value={this.state.destination}
+            name="destination"
+            onChange={this.handleChange}
+          >
+            <option value="">---Please select country---</option>
+            <option value="united states">United States</option>
+            <option value="india">India</option>
+            <option value="russia">Russia</option>
+            <option value="australia">Australia</option>
+            <option value="united kingdom">United Kingdom</option>
+          </select>
+        </label>
+        <br />
+        <label>
+          <input
+            type="checkbox"
+            name="isVegan"
+            checked={this.state.isVegan}
+            onChange={this.handleChange}
+          />
+          Vegan
+        </label>
+        <label>
+          <input
+            type="checkbox"
+            name="isLactoseFree"
+            checked={this.state.isLactoseFree}
+            onChange={this.handleChange}
+          />
+          Lacose Free
+        </label>
+        <label>
+          <input
+            type="checkbox"
+            name="isVegeterain"
+            checked={this.state.isVegeterain}
+            onChange={this.handleChange}
+          />
+          Vegeterian
+        </label>
+        <br />
+        <br />
         <label>Fovourite color</label>
-        <select
-          name="favcolor"
-          value={this.state.favcolor}
-          onChange={this.handleChange}
-        >
-          <option value="blue">Blue</option>
-          <option value="red">Red</option>
-          <option value="green">Green</option>
-          <option value="yellow">Yellow</option>
-        </select>
-        <h1>
+        <br />
+        <button>Submit</button>
+        <hr />
+        <br />
+        <h1>Entered Information:</h1>
+        <p>
+          Name:
           {this.state.firstName}
           {this.state.lastName}
-        </h1>
-        <h1>You are {this.state.gender}</h1>
-        <h2>Your Favourite color is {this.state.favcolor}</h2>
-        <button>Submit</button>
+        </p>
+        <p>You age is {this.state.age}</p>
+        <p>Your gender: {this.state.gender}</p>
+        <p>Destination: {this.state.destination}</p>
+        <p>
+          Your Dietary restriction: <br />
+          Vegan: {this.state.isVegan ? "Yes" : "No"} <br />
+          Lactose Free: {this.state.isLactoseFree ? "Yes" : "No"} <br />
+          Vegeterian: {this.state.isVegeterain ? "Yes" : "No"} <br />
+        </p>
       </form>
     );
   }
 }
-
-// class Header extends React.Component {
-//   render() {
-//     return (
-//       <header>
-//         <p>Welcome, {this.props.username}</p>
-//       </header>
-//     );
-//   }
-// }
-
-// class Greeting extends Component {
-//   render() {
-//     const date = new Date();
-//     const hours = date.getHours();
-//     let timeOfDay;
-//     if (hours < 12) {
-//       timeOfDay = "morning";
-//     } else if (hours >= 12 && hours <= 17) {
-//       timeOfDay = "afternoon";
-//     } else {
-//       timeOfDay = "night";
-//     }
-//     return <h1> Good {timeOfDay} to you. </h1>;
-//   }
-// }
 
 export default App;
